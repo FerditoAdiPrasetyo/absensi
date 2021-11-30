efb68b142f3ca9b0cfe089160d446c5b5eaec1c8
FerditoAdiPrasetyo <prasetyoadi126@gmail.com>
2021-11-30 11:55:35 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,21 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix'=>'master-data'], function (){
+    Route::get('/siswa', 'SiswaController@index')->name('master-data.siswa');
+});
+
+Route::get('login/github','GithubController@redirectToProvider');
+Route::get('login/github/callback','GithubController@handleProviderCallback');
+
+
+
+Route::group(['prefix' => 'manage'], function(){
+    route::get('/user','Manage\UserController@index')->name('manage.user');
+    route::get('/add/form/invite','Manage\UserController@create')->name('manage.add.form.invite');
+});
